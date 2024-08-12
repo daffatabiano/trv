0ed96af74cf5ce1b96dc7f6f057f1a2744fa9acc
daffa tabiano <daffatabianoo@gmail.com>
2024-08-12 11:05:48 +0700

    add(public-img) : general and logo picture

--- src/views/MainViews/index.jsx
+++ src/views/MainViews/index.jsx
@@ -7,20 +7,27 @@ export default function MainViews() {
         <BaseLayout>
             <div
                 className={cn(
-                    `text-3xl md:text-7xl font-bold  text-center`,
-                    styles.title
+                    'flex flex-col text-center justify-center items-center',
+                    styles.contain
                 )}
             >
-                <h1>
-                    E N J O <span> Y O U R </span>T - R A V E L
-                </h1>
+                <div
+                    className={cn(
+                        `text-3xl md:text-7xl font-bold  `,
+                        styles.contain__title
+                    )}
+                >
+                    <h1>
+                        E N J O <span> Y O U R </span>T - R A V E L
+                    </h1>
+                </div>
+                <div className="font-extralight text-base md:text-4xl dark:text-neutral-200 py-4">
+                    <p>Make Your Life Be More Fun</p>
+                </div>
+                <button className="bg-amber-300 rounded-full w-fit text-emerald-950 font-bold px-4 py-2 hover:bg-amber-400 hover:translate-y-1 hover:shadow-inner hover:shadow-amber-600 ">
+                    Explore Now
+                </button>
             </div>
-            <div className="font-extralight text-base md:text-4xl dark:text-neutral-200 py-4">
-                <p>Make Your Life Be More Fun</p>
-            </div>
-            <button className="bg-amber-300 rounded-full w-fit text-emerald-950 font-bold px-4 py-2 hover:bg-amber-400 hover:translate-y-1 hover:shadow-inner hover:shadow-amber-600 ">
-                Explore Now
-            </button>
         </BaseLayout>
     );
 }
