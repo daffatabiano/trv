e933d64b1e0fd267c5bac3f753d5d66b65b4816a
daffa tabiano <daffatabianoo@gmail.com>
2024-08-12 15:41:25 +0700

    feat(navbar) : scroll effect

--- src/layout/partials/Headers.jsx
+++ src/layout/partials/Headers.jsx
@@ -1,8 +1,31 @@
 import { Icons } from '@/components/Icons';
+import { useEffect, useState } from 'react';
 
 export default function Headers() {
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    useEffect(() => {
+        const stickyTop = document.querySelector('.sticky');
+        const handleScroll = () => {
+            if (window.scrollY >= 'sticky') {
+                setIsScrolled(true);
+            } else {
+                setIsScrolled(false);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
-        <nav className="flex gap-4 items-center bg-transparent sticky top-0 text-slate-950">
+        <nav
+            className={`flex gap-4 items-center ${
+                isScrolled ? 'bg-zinc-900 text-white' : 'bg - transparent'
+            } sticky top-0 text-slate-950 `}
+        >
             <div>
                 <Icons.Home />
             </div>
