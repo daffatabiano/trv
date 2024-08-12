4158924c533029096df3211a9ead38a35f9cf28e
daffa tabiano <daffatabianoo@gmail.com>
2024-08-12 17:35:43 +0700

    feat(main): page-views responsives

--- src/views/MainViews/index.jsx
+++ src/views/MainViews/index.jsx
@@ -16,11 +16,11 @@ export default function MainViews() {
                         duration: 0.8,
                         ease: 'easeInOut',
                     }}
-                    className="relative flex flex-col gap-4 items-center justify-center w-full"
+                    className="relative flex flex-col gap-0 items-center justify-center w-full"
                 >
                     <div
                         className={cn(
-                            `text-3xl md:text-7xl font-bold  `,
+                            `text-xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl font-bold  `,
                             styles.contain__title
                         )}
                     >
@@ -28,7 +28,7 @@ export default function MainViews() {
                             E N J O <span> Y O U R </span>T - R A V E L
                         </h1>
                     </div>
-                    <div className="font-extralight text-base md:text-4xl dark:text-neutral-200 py-4">
+                    <div className="font-extralight text-base text-lg md:text-4xl dark:text-neutral-200 py-4">
                         <p>Make Your Life Be More Fun</p>
                     </div>
                     <Button
