65a31adb05a29c2cd3097cd14effe7fb48cc1d5f
daffa tabiano <daffatabianoo@gmail.com>
2024-08-12 10:46:55 +0700

    feat(main): title-main

--- src/views/MainViews/index.jsx
+++ src/views/MainViews/index.jsx
@@ -1,13 +1,22 @@
 import BaseLayout from '@/layout/BaseLayout';
+import { cn } from '@/lib/utils';
+import styles from '@/views/MainViews/style.module.scss';
 
 export default function MainViews() {
     return (
         <BaseLayout>
-            <div className="text-3xl md:text-7xl font-bold dark:text-white text-center">
-                Background lights are cool you know.
+            <div
+                className={cn(
+                    `text-3xl md:text-7xl font-bold  text-center`,
+                    styles.title
+                )}
+            >
+                <h1>
+                    E N J O <span> Y O U R </span>T - R A V E L
+                </h1>
             </div>
             <div className="font-extralight text-base md:text-4xl dark:text-neutral-200 py-4">
-                And this, is chemical burn.
+                <p>Make Your Life Be More Fun</p>
             </div>
             <button className="bg-black dark:bg-white rounded-full w-fit text-white dark:text-black px-4 py-2">
                 Debug now
