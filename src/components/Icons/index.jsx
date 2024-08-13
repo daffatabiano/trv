c6bc4d6f416de60f746a190255775d060c6ed34f
daffa tabiano <daffatabianoo@gmail.com>
2024-08-13 15:01:24 +0700

    feat(drawer): features open-close

--- src/components/Icons/index.jsx
+++ src/components/Icons/index.jsx
@@ -58,7 +58,49 @@ const Hamburger = () => {
     );
 };
 
+const ChevronRight = () => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            className="lucide lucide-circle-chevron-right"
+        >
+            <circle cx="12" cy="12" r="10" />
+            <path d="m10 8 4 4-4 4" />
+        </svg>
+    );
+};
+
+const ChevronLeft = () => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            className="lucide lucide-circle-chevron-left"
+        >
+            <circle cx="12" cy="12" r="10" />
+            <path d="m14 16-4-4 4-4" />
+        </svg>
+    );
+};
+
 Icons.Home = Home;
 Icons.Promo = Promo;
 Icons.Top = Top;
 Icons.Hamburger = Hamburger;
+Icons.ChevronRight = ChevronRight;
+Icons.ChevronLeft = ChevronLeft;
--- src/layout/partials/Drawer.jsx
+++ src/layout/partials/Drawer.jsx
@@ -6,11 +6,23 @@ import { useEffect, useState } from 'react';
 const Sidebar = ({ lists, show }) => {
     return (
         <aside
-            id="sidebar"
             className={`w-full xs:w-1/4 gap-4  bg-amber-400 justify-between h-screen z-[100] fixed top-0 rounded-tr-3xl rounded-br-3xl ${show} transition-all duration-300`}
         >
-            <div className="w-full h-[25%]">
-                <img src="/img/logo/sec-logo.png" alt="logo-company" />
+            <div className="w-full flex h-[20%] justify-center  items-center">
+                <img
+                    src="/img/logo/sec-logo.png"
+                    className="w-full object-cover h-full"
+                    alt="logo-company"
+                />
+                <span
+                    className={`text-white font-bold w-fit text-center absolute text-sm p-2 top-2 right-2 rounded-full bg-amber-300 ${
+                        show === 'translate-x-0'
+                            ? '-translate-x-100'
+                            : 'translate-x-0'
+                    }`}
+                >
+                    x
+                </span>
             </div>
             <div className="w-full h-[50%]  px-2 py-8 flex flex-col gap-4 justify-between items-center">
                 {lists.map((item) => (
@@ -18,7 +30,7 @@ const Sidebar = ({ lists, show }) => {
                         <li>
                             <Link
                                 href={item.path}
-                                className="text-white flex items-center gap-2"
+                                className="text-white flex items-center gap-2 link"
                             >
                                 {item.icon} {item.name}
                             </Link>
@@ -34,22 +46,13 @@ const Sidebar = ({ lists, show }) => {
 };
 
 export default function Drawer() {
-    const [showDrawer, setShowDrawer] = useState('-translate-x-[100px]');
-
+    const [show, setShow] = useState(true);
     const handleClick = () => {
-        setShowDrawer('translate-x-0');
+        setShow((curr) => !curr);
     };
-
-    useEffect(() => {
-        const sidebar = document.querySelector('#sidebar');
-        sidebar.addEventListener('click', () => {
-            setShowDrawer('-translate-x-[100px]');
-        });
-    }, []);
 
     return (
         <>
-            <Sidebar show={showDrawer} lists={lists} />
             <nav
                 className={`${'flex'} w-full h-20 items-center justify-between z-20 backdrop-blur-md sticky top-0 text-slate-950 pe-4 pt-4`}
             >
@@ -59,22 +62,59 @@ export default function Drawer() {
                         onClick={handleClick}
                         id="btn-sidebar"
                     >
-                        <Icons.Hamburger />
+                        {show ? <Icons.ChevronRight /> : <Icons.ChevronLeft />}
                     </button>
                 </div>
                 <div className="w-1/6">
                     <img src="/img/logo/main-logo.png" alt="logo-company" />
                 </div>
             </nav>
-            <aside className=" w-[10%] fixed top-0 h-screen flex flex-col gap-4 justify-between py-36 items-start ">
-                {lists.map((item) => (
-                    <button
-                        className="bg-amber-400 px-2 py-8 rounded-tr-full rounded-br-full  hover:bg-amber-400 hover:translate-y-1 hover:shadow-inner hover:shadow-amber-600"
-                        key={item.id}
+            <aside
+                className={`w-full xs:w-1/2 gap-4  bg-amber-400 justify-between h-screen z-[100] fixed top-0 rounded-tr-3xl rounded-br-3xl transition-all duration-300`}
+            >
+                <div
+                    className={`w-full bg-red-200 flex h-[20%] ${
+                        show ? 'justify-center' : 'justify-between'
+                    } items-center transition-all duration-300`}
+                >
+                    {show ? (
+                        <img
+                            src="/img/logo/sec-logo.png"
+                            className={`overflow-hidden transition-all w-36 duration-300`}
+                            alt="logo-company"
+                        />
+                    ) : (
+                        <img
+                            src="/img/logo/single-logo.png"
+                            className={`overflow-hidden transition-all w-24 duration-300`}
+                            alt="logo-company"
+                        />
+                    )}
+
+                    <span
+                        className={`text-white font-bold w-fit text-center text-sm p-2 top-2 right-2 rounded-full bg-amber-300 `}
+                        onClick={handleClick}
                     >
-                        {item.icon}
-                    </button>
-                ))}
+                        {show ? <Icons.ChevronLeft /> : <Icons.ChevronRight />}
+                    </span>
+                </div>
+                <div className="w-full h-[50%]  px-2 py-8 flex flex-col gap-4 justify-between items-center">
+                    {lists.map((item) => (
+                        <ul key={item.id}>
+                            <li className="">
+                                <Link
+                                    href={item.path}
+                                    className="text-white flex items-center gap-2 link"
+                                >
+                                    {item.icon} {item.name}
+                                </Link>
+                            </li>
+                        </ul>
+                    ))}
+                </div>
+                <div className="w-full h-[10%] mt-24  justify-end items-end flex">
+                    <img src="/img/logo/main-logo.png" alt="logo-company" />
+                </div>
             </aside>
         </>
     );
