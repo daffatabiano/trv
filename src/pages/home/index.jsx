7ed4a0d7adbafb2a85e23cda9ef2efc94ec3b009
daffa tabiano <daffatabianoo@gmail.com>
2024-08-14 14:58:27 +0700

    add(page): home

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,9 @@
+import BaseLayout from '@/layout/BaseLayout';
+
+export default function Page() {
+    return (
+        <BaseLayout>
+            <h1>hello world!</h1>
+        </BaseLayout>
+    );
+}
