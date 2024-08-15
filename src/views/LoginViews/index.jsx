e448232c9b21f48e7a5ee8a74dd9565779c84749
daffa tabiano <daffatabianoo@gmail.com>
2024-08-15 07:51:36 +0700

    feat(components) : forms and ui slicing input

--- src/views/LoginViews/index.jsx
+++ src/views/LoginViews/index.jsx
@@ -1,7 +1,7 @@
-export default function Login () {
-    return (
+export default function Login() {
+    return (<div className="bg-slate-900/60 w-screen h-screen">
         <div>
             
         </div>
-    )
+    </div>);
 }
