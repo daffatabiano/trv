72d7cc7862d4d3f4b6d214fd023291b8c4c052c7
daffa tabiano <daffatabianoo@gmail.com>
2024-08-16 11:30:31 +0700

    feat(login): footers

--- src/views/LoginViews/index.jsx
+++ src/views/LoginViews/index.jsx
@@ -4,8 +4,8 @@ import Input, { InputPassword } from '@/components/ui/Input';
 
 export default function Login() {
     return (
-        <div className="bg-slate-900/60 w-screen h-screen flex items-center justify-center">
-            <div className="w-80 h-80 flex flex-col gap-4 items-center justify-center p-5">
+        <div className="bg-slate-900/60 w-screen h-screen flex flex-col items-center justify-center">
+            <div className="w-80 h-full flex flex-col gap-4 items-center justify-center p-5">
                 <img
                     src="/img/logo/single-logo.png"
                     alt="single-logo"
@@ -22,6 +22,16 @@ export default function Login() {
                         Login
                     </button>
                 </Form>
+            </div>
+            <div className="flex flex-col gap-2 text-center justify-end h-1/6 pb-5">
+                <ul className="text-white text-sm flex gap-2 ">
+                    <li>About Us</li>
+                    <li>Privacy Policy</li>
+                    <li>Terms and Conditions</li>
+                </ul>
+                <p className="text-white text-sm">
+                    © 2022. Acet Labs. All rights reserved.
+                </p>
             </div>
         </div>
     );
