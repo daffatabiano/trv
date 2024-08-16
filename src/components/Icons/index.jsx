73c58f4d371dc5e404c8dbe5d2ad33b45a71bfd8
daffa tabiano <daffatabianoo@gmail.com>
2024-08-16 17:20:14 +0700

    add(components): icons/ close-error-success

--- src/components/Icons/index.jsx
+++ src/components/Icons/index.jsx
@@ -263,6 +263,54 @@ const EyeOff = (w) => {
     );
 };
 
+const Success = (w) => {
+    return (
+        <svg
+            className={w}
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+        >
+            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+        </svg>
+    );
+};
+
+const Error = (w) => {
+    return (
+        <svg
+            className={w}
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+        >
+            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 11.793a1 1 0 1 1-1.414 1.414L10 11.414l-2.293 2.293a1 1 0 0 1-1.414-1.414L8.586 10 6.293 7.707a1 1 0 0 1 1.414-1.414L10 8.586l2.293-2.293a1 1 0 0 1 1.414 1.414L11.414 10l2.293 2.293Z" />
+        </svg>
+    );
+};
+
+const Close = (w) => {
+    return (
+        <svg
+            className={w}
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 14 14"
+        >
+            <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+            />
+        </svg>
+    );
+};
+
 Icons.Home = Home;
 Icons.Promo = Promo;
 Icons.Top = Top;
@@ -276,3 +324,6 @@ Icons.Dashboard = Dashboard;
 Icons.Cart = Cart;
 Icons.Eye = Eye;
 Icons.EyeOff = EyeOff;
+Icons.Success = Success;
+Icons.Error = Error;
+Icons.Close = Close;
