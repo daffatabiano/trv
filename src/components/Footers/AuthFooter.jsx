9e3d2b95a4e7587ae279e342356ffd0f3778f95e
daffa tabiano <daffatabianoo@gmail.com>
2024-08-17 22:01:44 +0700

    refact(footer): auth-footers login and register

---
+++ src/components/Footers/AuthFooter.jsx
@@ -0,0 +1,14 @@
+export default function AuthFooter() {
+    return (
+        <>
+            <ul className="text-stone-300 text-sm flex gap-2 ">
+                <li>About Us</li>
+                <li>Privacy Policy</li>
+                <li>Terms and Conditions</li>
+            </ul>
+            <p className="text-stone-300 text-sm">
+                © 2024. Daffa Tabiano. All rights reserved.
+            </p>
+        </>
+    );
+}
