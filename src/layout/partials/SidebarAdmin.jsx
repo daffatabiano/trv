803f9c52e5a6e018767bcdc18ef586361b1b75de
daffa tabiano <daffatabianoo@gmail.com>
2024-08-18 20:45:43 +0700

    fix(dashboard): color style

--- src/layout/partials/SidebarAdmin.jsx
+++ src/layout/partials/SidebarAdmin.jsx
@@ -12,19 +12,29 @@ export function SidebarAdmin(props) {
     const { children } = props;
     const [open, setOpen] = useState(false);
 
-    console.log(props, 'propss');
-
     return (
         <div
             className={cn(
-                'rounded-md flex flex-col md:flex-row bg-gray-100 dark:bg-neutral-800 w-full flex-1 max-w-7xl mx-auto border border-neutral-200 dark:border-neutral-700 overflow-hidden',
-                'h-[60vh]' // for your use case, use `h-screen` instead of `h-[60vh]`
+                'rounded-md flex flex-col md:flex-row bg-indigo-600 dark:bg-neutral-800 w-full flex-1 max-w-7xl mx-auto border border-indigo-200 dark:border-neutral-700 overflow-hidden',
+                'h-screen' // for your use case, use `h-screen` instead of `h-[60vh]`
             )}
         >
             <Sidebar open={open} setOpen={setOpen}>
                 <SidebarBody className="justify-between gap-10">
                     <div className="flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
-                        {open ? <Logo /> : <LogoIcon />}
+                        {open ? (
+                            <img
+                                src="/img/logo/sec-logo.png"
+                                alt="company-logo-opened"
+                                className="w-20 h-20"
+                            />
+                        ) : (
+                            <img
+                                src="/img/logo/single-logo.png"
+                                alt="company-logo"
+                                className="w-20 h-20 object-fill"
+                            />
+                        )}
                         <div className="mt-8 flex flex-col gap-2">
                             {lists.map((link, idx) => (
                                 <SidebarLink key={idx} link={link} />
@@ -89,7 +99,7 @@ export const LogoIcon = () => {
 const Dashboard = () => {
     return (
         <div className="flex flex-1">
-            <div className="p-2 md:p-10 rounded-tl-2xl border border-neutral-200 dark:border-neutral-700 bg-white dark:bg-neutral-900 flex flex-col gap-2 flex-1 w-full h-full">
+            <div className="p-2 md:p-10 rounded-tl-2xl border border-stone-200 dark:border-neutral-700 bg-white dark:bg-neutral-900 flex flex-col gap-2 flex-1 w-full h-full">
                 <div className="flex gap-2">
                     {[...new Array(4)].map((i) => (
                         <div
