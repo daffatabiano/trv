23fba808a6da4d3ffc8f666dbb1363ce260df908
daffa tabiano <daffatabianoo@gmail.com>
2024-08-19 08:47:18 +0700

    feat(dashboard): upload features => category

--- src/views/DashboardViews/CategoryViews/index.jsx
+++ src/views/DashboardViews/CategoryViews/index.jsx
@@ -6,11 +6,64 @@ export default function Category() {
     const [data, setData] = useState([]);
     const [token, setToken] = useState('');
 
+    const { post } = usePost();
+    const [imageUrl, setImageUrl] = useState({});
+
     useEffect(() => {
         if (typeof window !== 'undefined') {
             setToken(localStorage.getItem('token'));
         }
     }, []);
+
+    const uploadFile = async (e) => {
+        const file = e.target.files[0];
+
+        if (!file.type.startsWith('image')) {
+            setToast({
+                variant: 'error',
+                title: 'Upload Failed',
+                message: 'Format file should be .jpg/.png/.jpeg/.gif/.svg',
+                show: true,
+            });
+            return;
+        }
+
+        if (file.size > 1000000) {
+            setToast({
+                variant: 'error',
+                title: 'Too big size',
+                message: `Maximum upload 1 MB , your file size ${file.size}`,
+                show: true,
+            });
+            return;
+        } else {
+            setTimeout(async () => {
+                const newFile = new FormData();
+                newFile.append('image', file);
+
+                await upload('upload-image', newFile)
+                    .then((res) => {
+                        console.log(res);
+                        setToast({
+                            variant: 'success',
+                            title: 'Upload Success',
+                            message:
+                                'Your profile picture success to be applied',
+                            show: true,
+                        });
+                        setImageUrl(res.data.url);
+                    })
+                    .catch((err) => {
+                        setToast({
+                            variant: 'error',
+                            title: 'Upload Failed',
+                            message: err?.response?.message?.data,
+                            show: true,
+                        });
+                    });
+            }, 1000);
+        }
+    };
 
     const getCategory = async () => {
         const res = await getData('categories', token);
