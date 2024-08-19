961a80d4252cd175455dd24ee7a2e93af258dd97
daffa tabiano <daffatabianoo@gmail.com>
2024-08-19 08:14:58 +0700

    feat(profile-dashboard) : fetching data profile

--- src/views/DashboardViews/ProfileViews/index.jsx
+++ src/views/DashboardViews/ProfileViews/index.jsx
@@ -1,6 +1,30 @@
+import useGet from '@/hooks/useGet';
+import { useState, useEffect } from 'react';
+
 export default function Profile() {
+    const { getData } = useGet();
+    const [data, setData] = useState([]);
+    const [token, setToken] = useState('');
+
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            setToken(localStorage.getItem('token'));
+        }
+    }, []);
+
+    const getProfile = async () => {
+        const res = await getData('user', token);
+        setData(res);
+    };
+
+    console.log(Data);
+
+    useEffect(() => {
+        getProfile;
+    }, [token]);
+
     return (
-        <div>
+        <div className="bg-slate-500">
             <h1>Profile</h1>
         </div>
     );
