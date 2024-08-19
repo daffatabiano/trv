03f682c7fc376436c8947dd3a8991be13d70cf62
daffa tabiano <daffatabianoo@gmail.com>
2024-08-19 08:43:49 +0700

    feat(dashboard) : profile edit/update profile and upload picture featuring

--- src/hooks/usePost.js
+++ src/hooks/usePost.js
@@ -1,10 +1,10 @@
 import { BASE_HEADERS } from '@/services/BASE/data';
 
 export default function usePost() {
-    const post = async (url, body) => {
+    const post = async (url, body, token) => {
         try {
             const res = await axios.post(`${BASE_URL}/${url}`, body, {
-                headers: BASE_HEADERS,
+                headers: BASE_HEADERS(token),
             });
             return res;
         } catch (err) {
--- src/views/DashboardViews/ProfileViews/index.jsx
+++ src/views/DashboardViews/ProfileViews/index.jsx
@@ -1,16 +1,108 @@
 import useGet from '@/hooks/useGet';
+import usePost from '@/hooks/usePost';
 import { useState, useEffect } from 'react';
 
 export default function Profile() {
     const { getData } = useGet();
     const [data, setData] = useState([]);
     const [token, setToken] = useState('');
+    const [toast, setToast] = useState({});
+    const { post } = usePost();
+    const [imageUrl, setImageUrl] = useState({});
 
     useEffect(() => {
         if (typeof window !== 'undefined') {
             setToken(localStorage.getItem('token'));
         }
     }, []);
+
+    const uploadFile = async (e) => {
+        const file = e.target.files[0];
+
+        if (!file.type.startsWith('image')) {
+            setToast({
+                variant: 'error',
+                title: 'Upload Failed',
+                message: 'Format file should be .jpg/.png/.jpeg/.gif/.svg',
+                show: true,
+            });
+            return;
+        }
+
+        if (file.size > 1000000) {
+            setToast({
+                variant: 'error',
+                title: 'Too big size',
+                message: `Maximum upload 1 MB , your file size ${file.size}`,
+                show: true,
+            });
+            return;
+        } else {
+            setTimeout(async () => {
+                const newFile = new FormData();
+                newFile.append('image', file);
+
+                await upload('upload-image', newFile)
+                    .then((res) => {
+                        console.log(res);
+                        setToast({
+                            variant: 'success',
+                            title: 'Upload Success',
+                            message:
+                                'Your profile picture success to be applied',
+                            show: true,
+                        });
+                        setImageUrl(res.data.url);
+                    })
+                    .catch((err) => {
+                        setToast({
+                            variant: 'error',
+                            title: 'Upload Failed',
+                            message: err?.response?.message?.data,
+                            show: true,
+                        });
+                    });
+            }, 1000);
+        }
+    };
+
+    const editProfile = async (e) => {
+        try {
+            e.preventDefault();
+            const body = {
+                name: e.target.name.value,
+                email: e.target.email.value,
+                profilePictureUrl: imageUrl,
+                phoneNumber: e.target.phoneNumber.value,
+            };
+            const res = await post('update-profile', body, token);
+            if (res.status === 200) {
+                setToast({
+                    variant: 'success',
+                    title: 'Profile is changed',
+                    message: res.data.message,
+                    show: true,
+                });
+                setTimeout(() => {
+                    window.location.reload();
+                }, 3000);
+            } else {
+                setToast({
+                    variant: 'error',
+                    title: 'Edit Failed',
+                    message: res.response.data.message,
+                    show: true,
+                });
+            }
+        } catch (err) {
+            setToast({
+                variant: 'error',
+                title: 'Edit Failed',
+                message: err?.response?.data?.message,
+                show: true,
+            });
+        }
+    };
 
     const getProfile = async () => {
         const res = await getData('user', token);
