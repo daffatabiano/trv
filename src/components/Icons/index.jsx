0afa5699df388d257363ba01d1d8d90ad9c8e639
daffa tabiano <daffatabianoo@gmail.com>
2024-08-24 12:01:26 +0700

    feat(ui): dropdowns fix style conditional

--- src/components/Icons/index.jsx
+++ src/components/Icons/index.jsx
@@ -519,11 +519,24 @@ const CharretUp = ({ w }) => {
     );
 };
 
-const Filter =({w}) => {
+const Filter = ({ w }) => {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" width={} height={} viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-filter"><polygon points="22 3 2 3 10 12.46 10 19 14 21 14 12.46 22 3"/></svg>
-    )
-}
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width={w}
+            height={w}
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="lucide lucide-filter"
+        >
+            <polygon points="22 3 2 3 10 12.46 10 19 14 21 14 12.46 22 3" />
+        </svg>
+    );
+};
 
 Icons.Home = Home;
 Icons.Promo = Promo;
--- src/components/ui/Dropdowns/dropdown-dashboard-profile.jsx
+++ src/components/ui/Dropdowns/dropdown-dashboard-profile.jsx
@@ -11,18 +11,23 @@ export default function DropdownDashboardProfile(props) {
                     onClick={() => props?.setShow((prev) => !prev)}
                     className={`w-full py-2 ps-4 text-start font-bold  rounded-lg uppercase ${
                         props?.role
-                            ? (props?.role === 'admin'
+                            ? props?.role === 'admin'
                                 ? 'bg-emerald-300 text-emerald-600'
-                                : 'bg-rose-300 text-rose-600')
-                            : 'bg-transparent'
+                                : 'bg-rose-300 text-rose-600'
+                            : 'bg-transparent flex gap-2'
                     } `}
                 >
-                    {props?.value ? (props?.value ? props?.value : props?.role) : (
-    <>
-        <Icons.Filter w={24}/> Filter
-    </>
-)}
-
+                    {props?.value ? (
+                        props?.value ? (
+                            props?.value
+                        ) : (
+                            props?.role
+                        )
+                    ) : (
+                        <>
+                            <Icons.Filter w={24} /> Filter
+                        </>
+                    )}
                 </button>
                 <span
                     className="absolute right-4 top-0 translate-y-[40%] cursor-pointer"
@@ -40,7 +45,7 @@ export default function DropdownDashboardProfile(props) {
                     props?.show ? 'visible' : 'invisible'
                 }`}
             >
-              {props.children}
+                {props.children}
             </div>
         </>
     );
