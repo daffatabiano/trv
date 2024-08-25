5243f6bbd3885f4b910d1886e41de4b45fa450f5
daffa tabiano <daffatabianoo@gmail.com>
2024-08-25 13:23:51 +0700

    fix(register): fix style register button

--- src/views/RegisterViews/index.jsx
+++ src/views/RegisterViews/index.jsx
@@ -173,7 +173,7 @@ export default function RegisterViews() {
                     </p>
                     <button
                         type="submit"
-                        className="w-full bg-amber-300 rounded-full px-4 py-2 sm:w-1/2 sm:mx-auto text-white font-bold"
+                        className="w-full bg-amber-300 hover:bg-amber-400 hover:translate-y-1 hover:shadow-inner hover:shadow-amber-600 rounded-full px-4 py-2 sm:w-1/2 sm:mx-auto text-white font-bold"
                     >
                         Register
                     </button>
