d8731ed6cc724f70adcba301f316d7dba5ae24cd
daffa tabiano <daffatabianoo@gmail.com>
2024-08-26 15:38:46 +0700

    feat(banner): modified banner head

--- src/views/DashboardViews/BannerViews/index.jsx
+++ src/views/DashboardViews/BannerViews/index.jsx
@@ -11,7 +11,7 @@ export default function Banner() {
     const [sort, setSort] = useState('sort');
     const { post } = usePost();
     const [imageUrl, setImageUrl] = useState({});
-    const [bannerName, setBannerName] = useState('')
+    const [bannerName, setBannerName] = useState('');
 
     useEffect(() => {
         if (typeof window !== 'undefined') {
@@ -75,26 +75,34 @@ export default function Banner() {
     };
 
     const handleSort = () => {
-        setSort(sort === 'oldest' ? 'oldest' : 'newest');
+        setSort((value) => {
+            if (value === 'sort') {
+                return 'newest';
+            } else if (value === 'newest') {
+                return 'oldest';
+            } else if (value === 'oldest') {
+                return 'newest';
+            }
+        });
     };
 
     const addBanners = async () => {
         const body = {
             name: bannerName,
             imageUrl: imageUrl,
-        }
+        };
 
         const res = await post('create-banner', body, token);
-        console.log(res)
-    }
+        console.log(res);
+    };
 
     useEffect(() => {
         getBanner();
     }, [token]);
     return (
         <div className="rounded-lg overflow-hidden flex flex-col gap-4 w-full h-full">
-            <div className="w-full flex justify-between p-2 h-[15%] bg-amber-300 shadow-md shadow-amber-800/70">
-                <div className="flex flex-col p-2 w-[65%]">
+            <div className="w-full flex justify-between p-2 h-[15%] bg-amber-300 shadow-lg shadow-stone-400/70">
+                <div className="flex flex-col p-2 w-[60%]">
                     <h1 className="text-3xl text-amber-700 font-bold ">
                         Banner Control
                     </h1>
@@ -103,42 +111,49 @@ export default function Banner() {
                         or remove banners on your web pages.”
                     </p>
                 </div>
-                <div className="flex items-center justify-between px-2 w-[35%] py-2 gap-4">
-                    <div className="flex items-center gap-2">
+                <div className="flex items-center justify-between px-2 w-[40%] py-2 gap-4">
+                    <div className="flex items-center gap-4">
+                        <div className="flex items-center gap-1">
+                            <button
+                                type="button"
+                                className="flex gap-2 text-amber-700"
+                                onClick={handleSort}
+                            >
+                                {sort ? (
+                                    sort === 'newest' ? (
+                                        <Icons.CalendarArrowUp w={24} />
+                                    ) : (
+                                        <Icons.CalendarArrowDown w={24} />
+                                    )
+                                ) : (
+                                    <Icons.Calendar w={24} />
+                                )}
+                                {sort}
+                            </button>
+                            <button
+                                type="button"
+                                onClick={() => setSort('sort')}
+                                className="w-8 h-8 p-2 rounded-full text-amber-700 bg-amber-200/50 flex justify-center items-center"
+                            >
+                                <Icons.Refresh w={18} />
+                            </button>
+                        </div>
                         <button
                             type="button"
-                            className="text-amber-700 h-fit w-fit bg-amber-700/40 p-2 rounded-lg flex gap-2"
+                            className="text-amber-700 h-fit w-fit bg-amber-200/50 p-2 rounded-lg flex gap-2"
                         >
                             <Icons.Add w={24} />
                             Add New
-                        </button>
-                        <button
-                            type="button"
-                            className="flex gap-2 text-amber-700"
-                            onClick={handleSort}
-                        >
-                            {sort ? (
-                                sort === 'newest' ? (
-                                    <Icons.CalendarArrowUp w={24} />
-                                ) : (
-                                    <Icons.CalendarArrowDown w={24} />
-                                )
-                            ) : (
-                                <Icons.Calendar w={24} />
-                            )}
-                            {sort}
                         </button>
                     </div>
                     <BorderAnimation
                         borderRadius="1.75rem"
                         className={`w-full py-2 capitalize px-4 rounded-lg flex justify-between bg-amber-400/30 text-amber-600 `}
                         borderClassName={
-                            'bg-[radial-gradient(var(--amber-500)_40%,transparent_60%)]'
+                            'bg-[radial-gradient(var(--amber-600)_40%,transparent_60%)]'
                         }
                     >
-                        <span
-                            className={`font-extrabold pe-2 text-amber-600 $`}
-                        >
+                        <span className={`font-extrabold pe-2 text-amber-700 `}>
                             • {data?.length}{' '}
                         </span>{' '}
                         Banners Total
@@ -149,3 +164,7 @@ export default function Banner() {
         </div>
     );
 }
+
+export const AddBanners = () => {
+    return <div className="bg-amber-300 w-full h-[85%] "></div>;
+};
