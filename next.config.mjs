411e35b5b4ba0520734bbeca25417c13c41ed3f0
daffa tabiano <daffatabianoo@gmail.com>
2024-08-31 17:50:04 +0700

    add(next): configurasi next

--- next.config.mjs
+++ next.config.mjs
@@ -21,6 +21,12 @@ const nextConfig = {
                 port: '',
                 pathname: '/**',
             },
+            {
+                protocol: 'https',
+                hostname: 'i2.wp.com',
+                port: '',
+                pathname: '/**',
+            },
         ],
     },
 };
