da1a435575a86aa14e31bfac7faf6a6b10551a0f
unknown <daffatabianoo@gmail.com>
2024-09-04 16:45:39 +0700

    add(config): new config next

---
+++ next.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'travel-journal-api-bootcamp.do.dibimbing.id',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'cdn.pixabay.com',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'a.cdn-hotels.com',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'i2.wp.com',
+                port: '',
+                pathname: '/**',
+            },
+        ],
+    },
+};
