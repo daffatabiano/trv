55e16f5acd632bb243bf7fa622970e410f392855
unknown <daffatabianoo@gmail.com>
2024-09-09 20:08:12 +0700

    feat(comps): features modal with toastify for notificationc

--- src/components/modals/modal-delete.jsx
+++ src/components/modals/modal-delete.jsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from 'react';
 import { motion } from 'framer-motion';
 import useDelete from '@/hooks/useDelete';
+import Toast from '../ui/Toast';
 
 const ModalDelete = (props) => {
   const { title, show, setShow, id } = props;
   const [token, setToken] = useState('');
   const { deleteData } = useDelete();
+  const [toast, setToast] = useState({});
 
   console.log(id);
 
@@ -21,7 +23,24 @@ const ModalDelete = (props) => {
 
   const handleDelete = async () => {
     const res = await deleteData(`delete-${title}/${id}`, token);
-    console.log(res);
+    if (res?.status === 200) {
+      setToast({
+        variant: 'success',
+        title: 'Delete Success',
+        message: res?.data?.message,
+        show: true,
+      });
+      setTimeout(() => {
+        window.history.back();
+      }, 3000);
+    } else {
+      setToast({
+        variant: 'error',
+        title: 'Delete Failed',
+        message: res?.data?.message,
+        show: true,
+      });
+    }
   };
 
   return (
@@ -32,6 +51,7 @@ const ModalDelete = (props) => {
       className={`w-full h-full bg-zinc-950/20 fixed top-0 left-0 items-center justify-center z-50 ${
         show ? 'flex' : 'hidden'
       }`}>
+      <Toast {...toast} setToast={setToast} duration={3000} />
       <div className="w-64 h-64 rounded-lg bg-slate-100 overflow-hidden">
         <div className="w-full bg-slate-200">
           <h1 className="text-2xl p-2 text-center capitalize font-bold">
