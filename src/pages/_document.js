4f28eec92b4747463401a547471b8424db84b0cb
unknown <daffatabianoo@gmail.com>
2024-09-10 14:46:43 +0700

    fix(_docs): title page and logo

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,9 +1,13 @@
-import { Html, Head, Main, NextScript } from "next/document";
+/* eslint-disable @next/next/no-title-in-document-head */
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/img/logo/single-logo.png" />
+        <title>To Real Vacation</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
