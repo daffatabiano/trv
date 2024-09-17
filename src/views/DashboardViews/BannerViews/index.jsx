fa2b8e81bf86641ff8cb1566f358da66fcc9373e
unknown <daffatabianoo@gmail.com>
2024-09-17 14:15:47 +0700

    fix(banners): fix ui banner

--- src/views/DashboardViews/BannerViews/index.jsx
+++ src/views/DashboardViews/BannerViews/index.jsx
@@ -61,11 +61,12 @@ export default function Banner() {
 
 export const AddBanners = () => {
   const { post } = usePost();
-  const [imageUrl, setImageUrl] = useState({});
+  const [imageUrl, setImageUrl] = useState('');
   const [bannerName, setBannerName] = useState('');
   const [toast, setToast] = useState({});
   const { upload } = useUpload();
   const [token, setToken] = useState('');
+  const [hoverActive, setHoverActive] = useState(false);
 
   useEffect(() => {
     if (typeof window !== 'undefined') {
@@ -148,13 +149,26 @@ export const AddBanners = () => {
   };
 
   const removeImage = async () => {
-    setImageUrl({});
-    setToast({
-      variant: 'success',
-      title: 'Image Removed',
-      message: 'Banners picture success to removed!',
-      show: true,
-    });
+    if (imageUrl.length > 0) {
+      setImageUrl({});
+      setToast({
+        variant: 'success',
+        title: 'Image Removed',
+        message: 'Banners picture success to removed!',
+        show: true,
+      });
+    } else {
+      setToast({
+        variant: 'error',
+        title: 'No Image Found',
+        message: 'Please upload image first',
+        show: true,
+      });
+    }
+  };
+
+  const handleHover = () => {
+    setHoverActive(!hoverActive);
   };
 
   return (
@@ -175,24 +189,62 @@ export const AddBanners = () => {
           },
         }}>
         <div className="relative">
-          <div className="w-1/3 m-auto bg-white rounded-3xl flex flex-col justify-start h-48 items-center pb-6 absolute inset-0 -translate-y-48 pt-8">
-            <div className="w-full text-center text-2xl font-bold text-amber-800">
+          <div
+            onClick={handleHover}
+            className="md:w-1/3 w-[90%] m-auto bg-white rounded-3xl flex flex-col justify-start h-48 items-center pb-6 absolute inset-0 -translate-y-48 pt-8">
+            <div className="w-full justify-between px-4 text-center text-2xl flex font-bold text-amber-800">
               <h1>Add New Banner Form</h1>
+              <button
+                type="button"
+                onClick={() => window.history.back()}
+                className={`bg-transparent text-amber-800 text-sm font-light hover:underline underline-offset-4`}>
+                ← Back
+              </button>
             </div>
           </div>
-          <div className="w-1/3 m-auto bg-slate-100 rounded-3xl flex flex-col h-[435px] items-center p-4 absolute inset-0 hover:translate-y-2 -translate-y-10 transition-all">
-            <div className="w-full relative bg-white text-amber-400 border-amber-400 h-[35%] overflow-hidden border border-dashed rounded-3xl flex justify-center items-center py-4">
-              <p>📎</p>
-              <p>Attach your files</p>
+          <div
+            className={`md:w-1/3 w-[90%] m-auto bg-slate-100 rounded-3xl flex flex-col h-[435px] items-center p-4 absolute inset-0 ${
+              hoverActive || 'hover:' ? 'translate-y-2' : ''
+            } -translate-y-10 transition-all`}>
+            {imageUrl.length > 0 ? (
+              <img
+                src={imageUrl}
+                alt="banner"
+                className="w-full h-[35%] object-cover object-center rounded-3xl border border-dashed border-amber-400"
+              />
+            ) : (
+              <div className="w-full relative bg-white text-amber-400 border-amber-400 h-[35%] overflow-hidden border border-dashed rounded-3xl flex justify-center items-center py-4">
+                <p>📎</p>
+                <p>Attach your files</p>
+                <input
+                  type="file"
+                  onChange={(e) => uploadFile(e)}
+                  className="absolute w-full h-full opacity-1 py-4 flex justify-center items-center opacity-0"
+                />
+              </div>
+            )}
+            <div className="w-full flex justify-end">
+              <button
+                type="button"
+                onClick={() => removeImage()}
+                className="bg-transparent underlined text-md p-2 underlined text-rose-600">
+                clear
+              </button>
+            </div>
+            <div className="flex w-full mt-4 flex-col gap-2">
+              <label htmlFor="banner-name">Banner Name</label>
               <input
-                type="file"
-                className="absolute w-full h-full opacity-1 py-4 flex justify-center items-center opacity-0"
+                onChange={(e) => setBannerName(e.target.value)}
+                type="text"
+                placeholder="Banner Name"
+                className="w-full p-4 rounded-3xl border border-amber-300/80 focus:border-amber-400 focus:outline-none"
               />
             </div>
             <button
               type="button"
-              className=" w-full flex justify-end underlined text-md p-2 underlined text-rose-600">
-              clear
+              onClick={() => addBanners()}
+              className="w-full p-4 font-bold text-white bg-amber-400 rounded-3xl mt-8">
+              Add Banner
             </button>
           </div>
         </div>
